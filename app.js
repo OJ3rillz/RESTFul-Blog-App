23cd733b24ad32ffc18f815454d0e727a15e53ac
OJ3rillz <timitrilli@gmail.com>
2019-02-24 09:18:16 +0100

    add model config and app config

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ var bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
 express = require("express"),
 app = express();
+
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+      title: String,
+      image: String,
+      body: String,
+      created: {type: Date, default: Date.now}
+}); 
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//ROUTEFULL ROUTES
+
+
+app.listen(3000, function(){
+      console.log("Server has started!");
+  });
