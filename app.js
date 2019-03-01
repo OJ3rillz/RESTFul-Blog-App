e20608667fe2dbf2b4e6ff505669ede694227a4a
OJ3rillz <timitrilli@gmail.com>
2019-03-01 23:22:10 +0100

    update app.js

--- app.js
+++ app.js
@@ -25,12 +25,6 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create({
-         title: "Test Blog",
-         image: "https://images.unsplash.com/photo-1531845116688-48819b3b68d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-         body: "HELLO THIS IS A BLOG POST!"
-});
-
 //RESTFUL ROUTES
 app.get("/", function(req, res){
       res.redirect("/blogs");
